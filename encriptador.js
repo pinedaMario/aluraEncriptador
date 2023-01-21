7675549e2287f291a0f3b5082e0c2d41a4698b99
pinedaMario <mario.a.pineda@gmail.com>
2023-01-21 13:24:54 -0600

    output toggle behavior completed

--- encriptador.js
+++ encriptador.js
@@ -1,3 +1,13 @@
+var toggle = true;
+
 function noneTest(nombre) {
-  alert("boton presionado: " + nombre);
+  if (toggle) {
+    document.getElementById("initialOutput").style.setProperty("display", "none");
+    document.getElementById("resultOutput").style.setProperty("display", "block");
+  } else {
+    document.getElementById("initialOutput").style.setProperty("display", "table-cell");
+    document.getElementById("resultOutput").style.setProperty("display", "none");
+  }
+  toggle = !(toggle);
+  //alert("boton presionado: " + nombre);
 }
